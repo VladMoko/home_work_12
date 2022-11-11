016e4f9fa1e483e8b3b19838c12a0ef3a44000bf
m <advmoney2233@gmail.com>
2022-11-11 15:11:38 +0200

    Commit

--- src/firstTask/CheckTheTime.java
+++ src/firstTask/CheckTheTime.java
@@ -1,12 +1,36 @@
 package firstTask;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CheckTheTime {
 
     public static void main(String[] args) {
-        Thread thread = new UpDateTimeEverySecond();
-        Thread thread1 = new UpDateTimeEveryFiveSecond();
-        thread.start();
-        thread1.start();
-    }
+        AtomicInteger oneSecond = new AtomicInteger();
+        AtomicInteger fiveSecond = new AtomicInteger();
+        new Thread(() -> {
+            while (true){
+                System.out.println("Минуло " + oneSecond);
+                oneSecond.addAndGet(1);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        })
+                .start();
+        new Thread(() -> {
+            while (true){
+                System.out.println("Минуло " + fiveSecond);
+                fiveSecond.addAndGet(5);
 
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        })
+                .start();
+    }
 }
