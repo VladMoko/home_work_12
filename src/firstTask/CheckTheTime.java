8b6588c4b47e788c9362e7040aa65fcf97fae152
m <advmoney2233@gmail.com>
2022-11-12 13:55:46 +0200

    Commit

--- src/firstTask/CheckTheTime.java
+++ src/firstTask/CheckTheTime.java
@@ -1,36 +1,42 @@
 package firstTask;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CheckTheTime {
 
     public static void main(String[] args) {
-        AtomicInteger oneSecond = new AtomicInteger();
-        AtomicInteger fiveSecond = new AtomicInteger();
-        new Thread(() -> {
-            while (true){
-                System.out.println("Минуло " + oneSecond);
-                oneSecond.addAndGet(1);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        })
-                .start();
-        new Thread(() -> {
-            while (true){
-                System.out.println("Минуло " + fiveSecond);
-                fiveSecond.addAndGet(5);
+        AtomicInteger oneSecondPassed = new AtomicInteger();
+        AtomicInteger fiveSecondPassed = new AtomicInteger();
 
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Runnable oneSecond = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Passed " + oneSecondPassed);
+                oneSecondPassed.addAndGet(1);
             }
-        })
-                .start();
+        };
+
+        Runnable fiveSecond = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Passed " + fiveSecondPassed);
+                fiveSecondPassed.addAndGet(5);
+            }
+        };
+
+
+        ScheduledExecutorService executors = Executors.newScheduledThreadPool(2);
+        executors.scheduleAtFixedRate(oneSecond, 0, 1, TimeUnit.SECONDS);
+        executors.scheduleAtFixedRate(fiveSecond, 0, 5, TimeUnit.SECONDS);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executors.shutdown();
     }
 }
